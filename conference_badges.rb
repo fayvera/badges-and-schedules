02d3c3945112c3055d25e6d49d5e2d680912bb76
Fay Vera <fvera.cavalcante@gmail.com>
2021-01-13 21:29:46 -0500

    commit tree 1a45e288423f35372a89c4a1693c76d60352951e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require 'pry'
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    array_messages = []
+    attendees.each {|name|array_messages << badge_maker(name)}
+    array_messages
+end
+
+def assign_rooms(attendees)
+    array_messages = []
+    attendees.each_with_index do |name, index|
+        array_messages << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+    array_messages
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each{|names| puts names}
+    assign_rooms(attendees).each{|room| puts room}
+end
+
+
+
